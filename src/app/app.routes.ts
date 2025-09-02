a630d43363d34c9d780d2c19308248ce926475f0
Dacb2134 <77132959+Dacb2134@users.noreply.github.com>
2025-09-02 00:20:21 -0500

    refactor: centralize view routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,24 @@
-
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/views/home/home.component';
-import { NotFoundComponent } from './components/views/not-found/not-found.component';
+import { views } from './components/views/views.routes';
 
 export const routes: Routes = [
-    //pokemon
-     { path: 'Pokemon', loadChildren: () => import('./components/pokemon/pokemon.routes').then(m => m.pokemonroutes) },
-    //pokemonvs
-    { path: 'vs', loadChildren: () => import('./components/pokemonvs/pokemonvs.routes').then(m => m.pokemonvsroutes)},
-
-
-    //Homen
-    { path: 'home', component: HomeComponent },
-    { path: 'not-found', component: NotFoundComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', redirectTo: 'not-found' }
-
+  //pokemon
+  {
+    path: 'Pokemon',
+    loadChildren: () =>
+      import('./components/pokemon/pokemon.routes').then(
+        (m) => m.pokemonroutes
+      ),
+  },
+  //pokemonvs
+  {
+    path: 'vs',
+    loadChildren: () =>
+      import('./components/pokemonvs/pokemonvs.routes').then(
+        (m) => m.pokemonvsroutes
+      ),
+  },
+  ...views,
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'not-found' },
 ];
--- src/app/components/views/views.routes.ts
+++ src/app/components/views/views.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-
-export const views:Routes=[
-    {
-        path: 'Home',
-        component: HomeComponent
-      },
-    
-]
+export const views: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  // Additional view routes can be added here
+];
